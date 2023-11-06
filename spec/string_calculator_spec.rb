4888ec080551c1b58f4b0157dfb68cff4fc4e8ed
kazvee <kayvee123@gmail.com>
2023-11-05 17:34:51 -0700

    Add function so calculator works if given a single number in a string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,19 @@ describe StringCalculator do
         expect(StringCalculator.add("")).to eq(0)
       end
     end
+
+    context "single numbers" do
+      context "given '4'" do
+        it "returns 4" do
+          expect(StringCalculator.add("4")).to eql(4)
+        end
+      end
+
+      context "given '10'" do
+        it "returns 10" do
+          expect(StringCalculator.add("10")).to eql(10)
+        end
+      end
+    end
   end
 end
